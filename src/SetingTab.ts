a1a0150657b0fe62ba3a42b3cdb8f612fd49ff00
Postcount CI <postcount-ci@firegroup.io>
2023-03-01 13:37:56 +0700

    fix: Update version to 1.2.6

--- src/SetingTab.ts
+++ src/SetingTab.ts
@@ -91,7 +91,7 @@ export class SettingTab extends PluginSettingTab {
             text: `To reload Obsidian, you can use the menu "view -> Force reload" or "Reload App" in the command palette.`
         })
 
-        new Setting(containerEl)
+        /*new Setting(containerEl)
           .setName("Report Log")
           .setDesc("To improve this plugin, we want to collect some logs. ONLY logs that produced by Open Gate will be collected, you can check our source code on Github. You can disable this collection at any time in the setting tab.")
           .addToggle((toggle) => {
@@ -100,7 +100,7 @@ export class SettingTab extends PluginSettingTab {
               this.plugin.settings.allowErrorReport = value
               await this.plugin.saveSettings()
             })
-          })
+          })*/
 
         new Setting(containerEl)
             .setName('Follow me on Twitter')
