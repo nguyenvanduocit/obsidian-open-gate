b4473fa806fbee30d364334dc5ab1ca3f7da9bd0
WAR.FISH <617020+warfishe@users.noreply.github.com>
2023-09-20 08:08:20 +0200

    Update getDefaultUserAgent.ts
    Google Account Login Fix

--- src/fns/getDefaultUserAgent.ts
+++ src/fns/getDefaultUserAgent.ts
@@ -1,3 +1,3 @@
 export default function getDefaultUserAgent() {
-    return `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36`
+    return `Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0`
 }
