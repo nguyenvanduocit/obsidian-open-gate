fb43c63983902952b46fd1be25d385ee062ffab8
Duoc Nguyen <nguyenvanduocit@gmail.com>
2023-11-06 02:23:41 +0000

    Update getDefaultUserAgent.ts

--- src/fns/getDefaultUserAgent.ts
+++ src/fns/getDefaultUserAgent.ts
@@ -1,3 +1,3 @@
 export default function getDefaultUserAgent() {
-    return `Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0`
+    return `Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/117.0/HBpt3US8-18`
 }
