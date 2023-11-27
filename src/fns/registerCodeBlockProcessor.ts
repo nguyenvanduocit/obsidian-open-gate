ba4fe677ad8501bf53013020eb6d8f0dbbe5e72e
nguyenvanduocit+ <nguyenvanduocit@gmail.com>
2023-11-28 00:26:09 +0700

    Refactor code to use YAML format for gate syntax

--- src/fns/registerCodeBlockProcessor.ts
+++ src/fns/registerCodeBlockProcessor.ts
@@ -9,33 +9,45 @@ function processNewSyntax(sourceCode: string): Node {
     const url = sourceCode.split('\n')[0]
     sourceCode = sourceCode.replace(url, '').trim()
 
+    // Replace tabs with spaces at the start of each line, because YAML doesn't support tabs
+    sourceCode = sourceCode.replace(/^\t+/gm, (match) => '  '.repeat(match.length))
+
+    if (sourceCode.length === 0) {
+        return createFrame(createEmptyGateOption(), '800px')
+    }
+
+    let data
+    try {
+        data = parse(sourceCode)
+    } catch (error) {
+        return createErrorMessage()
+    }
+
+    if (typeof data !== 'object' || data === null || Object.keys(data).length === 0) {
+        return createErrorMessage()
+    }
+
     const options = createEmptyGateOption()
     options.url = url
 
     let height = '800px'
-
-    if (sourceCode.length !== 0) {
-        const data = parse(sourceCode)
-
-        if (typeof data !== 'object' || data === null || Object.keys(data).length === 0) {
-            return document.createTextNode(
-                "From version 1.10.0, the syntax of gate has changed. You have to use YAML format. Just like Obsidian's front matter. Sorry for the inconvenience."
-            )
-        }
-
-        if (data.height) {
-            // convert to string if it is number
-            if (typeof data.height === 'number') {
-                data.height = `${data.height}px`
-            }
-
-            height = data.height
-            delete data.height
-        }
-
-        Object.assign(options, data)
+    if (data.height) {
+        height = typeof data.height === 'number' ? `${data.height}px` : data.height
+        delete data.height
     }
 
+    Object.assign(options, data)
+
+    return createFrame(options, height)
+}
+
+function createErrorMessage(): Node {
+    return document.createTextNode(
+        "Starting from version 1.10.0, the syntax of 'gate' has been updated. You are now required to use the YAML format, similar to Obsidian's front matter. We apologize for any inconvenience this may cause."
+    )
+}
+
+function createFrame(options: GateFrameOption, height: string): HTMLIFrameElement | WebviewTag {
     let frame: HTMLIFrameElement | WebviewTag
 
     if (Platform.isMobileApp) {
